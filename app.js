2ced594d3e93dc84ae9116f9101b9d1daa243563
mehul-anand <89949898+mehul-anand@users.noreply.github.com>
2021-09-20 12:32:01 +0530

    check for negative values

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
-const billAmount = document.querySelector("#bill-amount")
-console.log(billAmount.value)
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const checkButton = document.querySelector("#check-button");
+errorMessage= document.querySelector("#error-message");
+checkButton.addEventListener("click",function cash (){
+    errorMessage.style.display="none";
+    if (billAmount.value > 0){
+        if (cashGiven.value >= billAmount.value){
+        }else{
+            errorMessageShow("Bill Amount cannot be greater than the cash given")
+        }
+    }else{
+        errorMessageShow("Bill Amount can't be negative")
+    }
+});
+
+function errorMessageShow(message){
+    errorMessage.style.display="block";
+    errorMessage.innerText=message;
+}
